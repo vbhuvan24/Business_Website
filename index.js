13865129b4dd0517af04e63b7936b7732b9d3226
Yash Verma <56463836+vbhuvan24@users.noreply.github.com>
2022-08-25 21:42:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let quick_links = document.querySelector(".quick_links")
+let social_icons = document.querySelector(".social-media-flot")
+
+function social_icons_f() {
+    if (social_icons.offsetTop <= quick_links.offsetTop) {
+
+    }
+    else {
+
+    }
+}
+window.onscroll = () => {
+    social_icons_f()
+}
+social_icons_f()
